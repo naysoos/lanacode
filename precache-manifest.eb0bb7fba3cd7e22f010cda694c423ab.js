590b06787637a095879517865bc1776a7e7611ee
naysoos <165573226+naysoos@users.noreply.github.com>
2024-10-08 12:31:16 +0200

    Add files via upload

---
+++ precache-manifest.eb0bb7fba3cd7e22f010cda694c423ab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4427aea91fab654653e7",
+    "url": "/static/css/main.66fd59fb.chunk.css"
+  },
+  {
+    "revision": "4427aea91fab654653e7",
+    "url": "/static/js/main.890bb54c.chunk.js"
+  },
+  {
+    "revision": "2170416772c805c12c12",
+    "url": "/static/js/runtime~main.8b7c6f9e.js"
+  },
+  {
+    "revision": "e09c16ebbc6e10dd8883",
+    "url": "/static/js/2.cc54b962.chunk.js"
+  },
+  {
+    "revision": "069d0855611b0b42bfc5",
+    "url": "/static/js/3.fcc1ce91.chunk.js"
+  },
+  {
+    "revision": "df3ee2e2ba71b85471808d3708c2f5be",
+    "url": "/static/media/logo.df3ee2e2.png"
+  },
+  {
+    "revision": "5ed50ead971f45f72c325631f9682fb2",
+    "url": "/static/media/m4code.5ed50ead.png"
+  },
+  {
+    "revision": "390f9abaa425f97fba9e24c39fe8abb6",
+    "url": "/index.html"
+  }
+];
